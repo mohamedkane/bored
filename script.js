df8590d73565735a76ed2060c07dede454f28bc8
mohamedkane <43444237+mohamedkane@users.noreply.github.com>
2019-03-26 09:33:07 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+
+$(".Taka").dblclick(function() {
+    $(".dollar").hide();
+});
+
+$(".naira").click(function() {
+    $("#sun").text("subscribe to cakeo smakeo amd play yugioh duel links");
+});
+
+$("#quarter").hide();
+$("#euro").hover(function() {
+    $("#quarter").show();
+});
